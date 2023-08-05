34ef03cf0b3cf38dcc32f17885c3313b78806067
haseena-pa <haseenakhader.uk@gmail.com>
2023-08-05 11:25:55 +0100

    removed button component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'basic-primeng-angular-16';
+  title = 'primeng-angular-16';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ButtonModule } from 'primeng/button';
 
 import { AppComponent } from './app.component';
 
@@ -9,8 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    ButtonModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
