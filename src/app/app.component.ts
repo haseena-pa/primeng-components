6fdc3aac41c8d398cba8a0560ccc3f820062df04
haseena-pa <haseenakhader.uk@gmail.com>
2023-08-17 18:15:46 +0100

    chart component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'primeng-angular-16';
+export class AppComponent implements OnInit {
+
+  basicData: any;
+  basicOptions: any;
+
+  ngOnInit() {
+    this.basicData = {
+      labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+      datasets: [
+        {
+          label: 'First Dataset',
+          data: [10, 20, 30, 40],
+        }
+      ]
+    };
+    this.basicOptions = {
+      indexAxis: 'y',
+      elements: {
+        bar: {
+          backgroundColor: 'red'
+        }
+      }
+    }
+  }
+
+  selectData(event: any) {
+    console.log(event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChartModule } from 'primeng/chart';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
