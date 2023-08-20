c3ef000687fc535f5b3299b352e00126ac23f9fe
haseena-pa <haseenakhader.uk@gmail.com>
2023-08-20 17:26:48 +0100

    primeng menubar component changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'primeng-angular-16';
+export class AppComponent implements OnInit {
+
+  items: MenuItem[] | undefined;
+  ngOnInit() {
+    this.items = [
+      {
+        label: '<b>File</b>',
+        escape: false,
+        items: [{
+          label: 'New',
+          icon: 'pi pi-fw pi-plus',
+          items: [
+            { label: 'Project', url: 'https://www.google.com' },
+            { label: 'Other', command: () => this.someFn() },
+          ]
+        },
+        { label: 'Open' },
+        { label: 'Quit' }
+        ]
+      },
+      {
+        label: 'Edit',
+        icon: 'pi pi-fw pi-pencil',
+        items: [
+          { label: 'Delete', icon: 'pi pi-fw pi-trash' },
+          { label: 'Refresh', icon: 'pi pi-fw pi-refresh' }
+        ]
+      }
+    ];
+  }
+
+  someFn() {
+    console.log('hi')
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MenubarModule } from 'primeng/menubar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MenubarModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
