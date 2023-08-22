2a4db46a355d1a12c9ed1d44880bd182a81254e3
haseena-pa <haseenakhader.uk@gmail.com>
2023-08-22 19:31:55 +0100

    input switch component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'primeng-angular-16';
+  myGroup = new FormGroup({
+    switchChecked: new FormControl<string>('yes')
+  });
+
+  handleEvent(e: any) {
+    console.log(e)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputSwitchModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
