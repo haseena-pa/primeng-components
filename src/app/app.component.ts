f1fe79e5149db7b48be9d211f908e2799c09b364
haseena-pa <haseenakhader.uk@gmail.com>
2023-08-27 20:34:03 +0100

    primeng toast component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [MessageService]
 })
 export class AppComponent {
-  title = 'primeng-angular-16';
+  title = 'primeng-toast';
+
+  constructor(private messageService: MessageService) { }
+
+  showToast() {
+    this.messageService.add({ key: "c", severity: 'success', summary: 'Heading', detail: 'Are you sure?' });
+
+  }
+
+  clearToast() {
+    this.messageService.clear("bleft");
+  }
+
+  onConfirm() {
+    console.log('Confirm Clicked');
+    this.messageService.clear("c");
+  }
+
+  onReject() {
+    this.messageService.clear("c");
+  }
 }
